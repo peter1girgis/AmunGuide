8e10ad5f1c5d9d59fe2327af3de89aee482b8aa6
peter1girgis <petergirges71@gmail.com>
2026-06-22 13:11:40 +0300

    Restrict allowed origins in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+    'https://amun-guide-web.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
